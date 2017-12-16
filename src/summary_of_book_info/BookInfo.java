4ef3e605a21cf4c3c2aef07560dc7fe762a2d83c
Nazmul Hasan <nhrhandsome@gmail.com>
2017-12-16 13:39:23 -0500

    This is my first assignment to Web Project 1001

---
+++ src/summary_of_book_info/BookInfo.java
@@ -0,0 +1,26 @@
+//*****************************************************
+//BookInfo.java   Author: Nazmul Hasan
+// Demonstrates the basic structure of java application.
+//*******************************************************
+
+package summary_of_book_info;
+
+public class BookInfo {
+
+	public static void main(String[] args) {
+		System.out.print("Title:");
+	    System.out.println("   Secrets of Lost Summer");
+	    System.out.print("Author:");
+	    System.out.println("           Carla Neggers");
+	    System.out.print("Published:");
+	    System.out.println("                 2012");
+	    System.out.print("Pages:");
+	    System.out.println("                      346");
+	    System.out.print("Favorite Character:");
+	    System.out.println("      Olivia");
+	    System.out.print("Has a movie:");
+	    System.out.println("                 No");
+
+	}
+
+}
