9dddfda006d83324e6a3d9d427738d634b59eb14
Nazmul Hasan <nhrhandsome@gmail.com>
2017-12-16 13:52:27 -0500

    I have uploaded another file

---
+++ src/design_with_stars/Design.java
@@ -0,0 +1,41 @@
+//*****************************************************
+//Design.java   Author: Nazmul Hasan
+// Demonstrates the basic structure of java application.
+//*******************************************************
+
+
+package design_with_stars;
+
+public class Design {
+
+	public static void main(String[] args) {
+		System.out.println("************");
+		  System.out.println(" *        * ");
+		  System.out.println("  *      *  ");
+		  System.out.println("   ******   ");
+		  System.out.println("   ******   ");
+		  System.out.println("  *      *  ");
+		  System.out.println(" *        * ");
+		  System.out.println("************");
+		  System.out.println("");
+		  System.out.println("              **********");
+		  System.out.println("               *      * ");
+		  System.out.println("                *    *  ");
+		  System.out.println("                 ****   ");
+		  System.out.println("                 ****   ");
+		  System.out.println("                *    *  ");
+		  System.out.println("               *      * ");
+		  System.out.println("              **********");
+		  System.out.println("");
+		  System.out.println("    ********");
+		  System.out.println("     *    * ");
+		  System.out.println("      *  *  ");
+		  System.out.println("       **   ");
+		  System.out.println("       **   ");
+		  System.out.println("      *  *  ");
+		  System.out.println("     *    * ");
+		  System.out.println("    ******** ");
+
+	}
+
+}
